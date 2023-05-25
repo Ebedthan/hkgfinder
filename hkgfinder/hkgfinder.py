1e921829c8a2a1c2a55da8cec678d2f4d371c6dc
Anicet Ebou <anicet.ebou@gmail.com>
2023-05-25 04:20:04 +0000

    fix: rm unnecessary utils

--- hkgfinder/hkgfinder.py
+++ hkgfinder/hkgfinder.py
@@ -10,6 +10,8 @@ import logging
 import os
 import platform
 import sys
+import typing
+import warnings
 from collections import namedtuple
 from datetime import datetime
 from operator import attrgetter
@@ -24,8 +26,7 @@ import pyrodigal
 import xphyle
 import xphyle.paths
 from Bio import SeqIO
-
-from . import utils
+from Bio.Seq import reverse_complement, translate
 
 AUTHOR = "Anicet Ebou <anicet.ebou@gmail.com>"
 URL = "https://github.com/Ebedthan/hkgfinder"
@@ -132,6 +133,62 @@ HMMDESC = {
     "nifH": "nitrogenase iron protein",
     "nodC": "chitooligosaccharide synthase NodC",
 }
+
+
+def _translate_seq(seq: str) -> typing.List[str]:
+    """
+    _translate_seq translate DNA sequence to proteins in the six frames.
+
+    :seq: DNA sequence to translate.
+    """
+
+    seqlist = []
+
+    rev_comp = reverse_complement(seq)
+
+    # frame 1
+    seqlist.append(translate(seq))
+    # frame 2
+    seqlist.append(translate(seq[1:]))
+    # frame 3
+    seqlist.append(translate(seq[2:]))
+    # frame 4
+    seqlist.append(translate(rev_comp))
+    # frame 5
+    seqlist.append(translate(rev_comp[1:]))
+    # frame 6
+    seqlist.append(translate(rev_comp[2:]))
+
+    return seqlist
+
+
+def do_translation(infile: str, outfile: Path, width=60):
+    """
+    do_translation translate a DNA fasta file into proteins
+    fasta file.
+
+    :infile: Pyfasta object.
+    :outfile: Output file.
+    :sw: Sequence width. Default: 60.
+    """
+
+    file = pyfastx.Fasta(infile)
+
+    with open(outfile, "w", encoding="utf-8") as protfile:
+        for sequence in file:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                protseq = _translate_seq(sequence.seq)
+                for idx, frame in enumerate(protseq):
+                    seq_letters = [
+                        frame[i : i + width]
+                        for i in range(0, len(frame), width)
+                    ]
+                    new_line = "\n"
+                    protfile.write(
+                        f">{sequence.name}_frame={idx + 1}\n"
+                        + f"{new_line.join(map(str, seq_letters))}\n"
+                    )
 
 
 def main():
@@ -315,7 +372,7 @@ def main():
                 logging.info("Translating sequences into 6 frames")
 
                 # translate sequences
-                utils.do_translation(
+                do_translation(
                     str(Path(tmpdir, "hkgfinder_input.fa")),
                     Path(tmpdir, "input_translate.fa"),
                 )
